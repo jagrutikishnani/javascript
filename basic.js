4595a2590e51998391dc4504075bb75ff52b5844
jagrutikishnani <jagrutikevlani27@gmail.com>
2025-09-11 17:00:02 +0000

    datatype

---
+++ basic.js
@@ -0,0 +1,5 @@
+console.log("hi")
+x =10 
+y =20 
+z=30
+console.table([x,y,z])
---
+++ datatype.js
@@ -0,0 +1,29 @@
+"use strict" // it shows dat you are using newer virsion of java script
+let name = "meher"
+let age = 32
+let accountid = null
+console.log(accountid)
+
+
+console.log(3+ 3) ; console.log("meher")  //code read ability should be high
+
+
+//data types
+
+// number - range between  2 to power of 53 
+// bigint
+// string
+// boolean  - true/false
+// undefind
+// null
+// symbol - unique
+
+
+//type of keyword  
+
+// it shows type of any variable or datatype of any objects
+
+console.log(typeof null)  // - object
+console.log(typeof age)
+console.log(typeof undefined) //  - undefined
+ 
